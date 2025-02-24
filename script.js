6dbdfeee83a41bc6f3a677f14e205e751d5a0813
CynthiaEagen <eagen.cynthia@gmail.com>
2025-02-24 00:03:22 -0500

    Added basic editing capability.

--- script.js
+++ script.js
@@ -11,8 +11,11 @@ class Thing {
 const thingArray = [];
 const list = document.getElementById('list'); 
 
+const idView = document.getElementById('id-view');
+const nameView = document.getElementById('name-view');
+const notesView = document.getElementById('notes-view');
+
 function addThing() {
-    console.log("ping");
     let thing = new Thing(
         document.getElementById('name').value,
         document.getElementById('notes').value
@@ -29,12 +32,23 @@ function addThing() {
 }
 
 function viewThing() {
-    let nameView = document.getElementById('name-view');
-    let notesView = document.getElementById('notes-view');
-
+    idView.value = this.id;
     nameView.value = thingArray[this.id].name;
-    notesView.innerHTML = thingArray[this.id].notes; 
+    notesView.value = thingArray[this.id].notes; 
+}
+
+function editThing() {
+    let thing = thingArray[idView.value];
+    thing.name = nameView.value;
+    thing.notes = notesView.value;
+    thingArray[idView.value] = thing;
+
+    let li = document.getElementById(idView.value);
+    li.innerText = thing.name;
 }
 
 const submit = document.getElementById('submit-button');
 submit.addEventListener('click', addThing);
+
+const edit = document.getElementById('edit-button');
+edit.addEventListener('click', editThing);
